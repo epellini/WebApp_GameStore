7141cdc81fbe58c206d3f7ab6dc0e5cdc6ecd1bf
Nikita <nlitvinov924@gmail.com>
2023-10-01 17:17:31 -0400

    Add frontend views for multiple pages
    - Created frontend views for Login, Signup, Locked, ForgotPassword, and RegistrationSuccess pages.
    - Integrated Bootstrap styles for consistency and responsiveness.
    - Added user feedback messages like account locked notification and successful registration.
    - Note: Backend logic for handling locked accounts and password recovery needs to be implemented."

---
+++ VirtualGameStore/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualGameStore.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+      
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        // Register/Signup
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("SignUp");
+        }
+
+        // Forgot Password
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View("ForgotPassword");
+        }
+
+        //Registration Success page
+        //It's going to be shown to the user after email confirmation
+        [HttpGet]
+        public IActionResult Success()
+        {
+            return View("Success");
+        }
+
+        //Locked
+        [HttpGet]
+        public IActionResult Locked()
+        {
+            return View("Locked");
+        }
+    }
+}
