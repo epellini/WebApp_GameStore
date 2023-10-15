2d534857a2a19f5ae6ce0d47c5dfbb6fc0c7bdbb
John Magvas <jmagvas2944@conestogac.on.ca>
2023-10-15 18:19:14 -0400

    Add signup

--- VirtualGameStore/Controllers/AccountController.cs
+++ VirtualGameStore/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using VirtualGameStore.Entities;
+using VirtualGameStore.Models;
 
 namespace VirtualGameStore.Controllers
 {
@@ -39,18 +42,126 @@ namespace VirtualGameStore.Controllers
             return RedirectToAction("Success");
         }
 
+        // GET:
+        // /account/signup
+        [HttpGet("account/signup")]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("SignUp");
+        }
+
+        // POST:
+        // /account/signup
+        [HttpPost("account/signup")]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            // If the form is not valid, return the view with the model (showing errors, see RegisterViewModel):
+            if (!ModelState.IsValid)
+            {
+                return View("Signup", model);
+            }
+
+            // If the form is valid create a new user with the given name and email:
+            var user = new User
+            {
+                UserName = model.Username,
+                Email = model.Email
+            };
+
+            // Use the userManager to create a new user with the given password:
+            // This adds the user to the AspNetUsers table
+            // Save the result to a variable to check if the operation was successful:
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            // If the operation was not successful, add the errors to the model and return the view:
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                // This will display the error messages in the validation summary:
+                return View("Signup", model);
+            }
+
+            // If the operation was successful, redirect to method that sends email:
+            return RedirectToAction("SendVerification", new { email = user.Email });
+        }
+
+        [HttpGet("account/verify/{email}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> SendVerification(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.Action("ConfirmEmail", "Account", new { token, email = user.Email }, Request.Scheme);
+                string body = string.Format("<h2>Welcome to CVGS!</h2><hr>" +
+                    $"<p><b>Hi {user.UserName.Substring(0, 1).ToUpper()}{user.UserName.Substring(1).ToLower()},</b></p><br>" +
+                    $"<p>Thank you for signing up!</p>" +
+                    $"<p>Click <a href=\"{confirmationLink}\">here</a> to verify your email address.</p>");
+                await _emailSender.SendEmailAsync(user.Email, "Verify Your Email Address", body);
+                return RedirectToAction("SuccessRegistration", "Account", new { email = user.Email });
+            }
+
+            ViewBag.errorMessage = "Unfortunately, we were unable to validate your account details.";
+            return View("Error");
+        }
+
+        // GET:
+        // /Account/SuccessRegistration
+        [HttpGet("account/success")]
+        [AllowAnonymous]
+        public IActionResult SuccessRegistration(string email)
+        {
+            if (!string.IsNullOrEmpty(email))
+            {
+                return View("SuccessRegistration", email);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET:
+        // /Account/ConfirmEmail
+        [HttpGet("account/confirm")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    return View("SuccessVerification");
+                }
+                else
+                {
+                    ViewBag.errorMessage = "Unfortunately, we were unable to process your email confirmation request. Please try again.";
+                }
+            }
+            else
+            {
+                ViewBag.errorMessage = "Unfortunately, we were unable to validate your account details.";
+            }
+            return View("Error");
+        }
+
+        [HttpGet("account/login")]
         public IActionResult Login()
         {
             return View();
         }
 
-
-        // Register/Signup
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View("SignUp");
-        }
 
         // Forgot Password
         [HttpGet]
@@ -72,6 +183,49 @@ namespace VirtualGameStore.Controllers
         public IActionResult Locked()
         {
             return View("Locked");
+        }
+
+        [HttpGet("account/error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> CheckEmail(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return Json(true);
+            }
+            else
+            {
+                return Json("That email is registered to an account.");
+            }
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> CheckUsername(string username)
+        {
+            Regex emailrx = new Regex(@"(?i)^[a-z0-9]+$");
+            if (!emailrx.IsMatch(username))
+            {
+                return Json("Username cannot have spaces or special characters.");
+            }
+            else
+            {
+                var user = await _userManager.FindByNameAsync(username);
+                if (user == null)
+                {
+                    return Json(true);
+                }
+                else
+                {
+                    return Json("That username is taken.");
+                }
+            }
         }
 
         // private fields for services
