55da44e876b39488ed1c3215cd888d1a6f9e881b
John Magvas <jmagvas2944@conestogac.on.ca>
2023-11-12 13:57:26 -0500

    Add FriendConnect entity and update DB

---
+++ VirtualGameStore/Entities/FriendConnect.cs
@@ -0,0 +1,19 @@
+﻿namespace VirtualGameStore.Entities
+{
+    public class FriendConnect
+    {
+        public int FriendConnectId { get; set; }
+
+        // Foreign key properties:
+        public string UserId { get; set; }
+        public string FriendId { get; set; }
+
+        // Navigation properties:
+        public User? User { get; set; }
+        public User? Friend { get; set; }
+
+        // Properties
+        public DateTime? DateConnected { get; set; }
+        public string? Status { get; set; }
+    }
+}
