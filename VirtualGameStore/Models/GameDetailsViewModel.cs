1ed5c7c223fa6e2bf1597679680b7930d617c76d
JMichalsky <113627972+JMichalsky@users.noreply.github.com>
2023-11-12 16:43:45 +0100

    add backend work for game details page

---
+++ VirtualGameStore/Models/GameDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using VirtualGameStore.Entities;
+
+namespace VirtualGameStore.Models
+{
+    public class GameDetailsViewModel
+    {
+        // Properties:
+        public int GameId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Developer { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public double? RetailPrice { get; set; }
+        public ICollection<Picture>? PictureUrl { get; set; }
+        public string? Genres { get; set; }
+        public string? Languages { get; set; }
+        public string? Platforms { get; set; }
+    }
+}
